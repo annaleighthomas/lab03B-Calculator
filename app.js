e076eee0b7874dadb28b3a8b22100fbffb845d29
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-02-11 14:21:09 -0500

    working calculator

--- app.js
+++ app.js
@@ -1,5 +1,69 @@
-// import functions and grab DOM elements
 
-// initialize state
+const numberOne = document.getElementById('number-one');
+const numberTwo = document.getElementById('number-two');
+const solveButton = document.getElementById('solve-button');
+const solution = document.getElementById('solution');
 
-// set event listeners to update state and DOM
+console.log(numberOne, numberTwo, solveButton, solution);
+
+solveButton.addEventListener('click', () => {
+    
+    const addTotal = Number(numberOne.value) + Number(numberTwo.value);
+
+    solution.textContent = addTotal;
+
+    console.log(addTotal);
+});
+
+const numberOneSub = document.getElementById('one-sub');
+const numberTwoSub = document.getElementById('two-sub');
+const solveButtonSub = document.getElementById('sub-button');
+const solutionSub = document.getElementById('sub-solution');
+
+console.log(numberOneSub, numberTwoSub, solveButtonSub, solutionSub);
+
+solveButtonSub.addEventListener('click', () => {
+    
+    const addTotalSub = Number(numberOneSub.value) - Number(numberTwoSub.value);
+
+    solutionSub.textContent = addTotalSub;
+
+    console.log(addTotalSub);
+
+});
+
+const numberOneDiv = document.getElementById('one-div');
+const numberTwoDiv = document.getElementById('two-div');
+const solveButtonDiv = document.getElementById('div-button');
+const solutionDiv = document.getElementById('div-solution');
+
+console.log(numberOneDiv, numberTwoDiv, solveButtonDiv, solveButtonDiv);
+
+solveButtonDiv.addEventListener('click', () => {
+
+    const addTotalDiv = Number(numberOneDiv.value) / Number(numberTwoDiv.value);
+
+    solutionDiv.textContent = addTotalDiv;
+
+    console.log(addTotalDiv);
+});
+
+const numberOneMult = document.getElementById('one-mult');
+const numberTwoMult = document.getElementById('two-mult');
+const solveButtonMult = document.getElementById('mult-button');
+const solutionMult = document.getElementById('mult-solution');
+
+console.log(numberOneMult, numberTwoMult, solveButtonMult, solutionMult);
+
+solveButtonMult.addEventListener('click', () => {
+
+    const addTotalMult = Number(numberOneMult.value) * Number(numberTwoMult.value);
+
+    solutionMult.textContent = addTotalMult;
+
+    console.log(addTotalMult);
+});
+
+
+
+
